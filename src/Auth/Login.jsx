b9b5b54d33b59ad2118f6e309273f80fcf7f6ae2
Rohit Rajput <rajputrohitbcs@gmail.com>
2024-11-13 13:23:39 +0530

    local

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -36,6 +36,9 @@ const Login = () => {
       dispatch(setLoading(true));
       const res = await axios.post(`https://careernestbackend.onrender.com/api/v1/user/login`, input,{withCredentials:true});
       if (res.data.sucess) {
+        const { token } = res.data.success;
+        localStorage.setItem('token', token);
+        localStorage.getItem('token');
       dispatch(setUser(res.data.user))
         navigate("/");
         toast.success(res.data.message);
