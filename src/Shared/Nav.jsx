898089c1a5be8322df73f2328028c3eebb447870
Rohit Rajput <rajputrohitbcs@gmail.com>
2024-11-29 18:21:35 +0530

    logo

--- src/Shared/Nav.jsx
+++ src/Shared/Nav.jsx
@@ -11,6 +11,7 @@ import { LogOut, User2 } from "lucide-react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import carrernest from '../Assets/carrernest.jpg'
 //import React from 'react'
 
 const Nav = () => {
@@ -43,11 +44,14 @@ toast.success(res.data.message);
   <path fill="#006666" d="M0,192L60,186.7C120,181,240,171,360,176C480,181,600,203,720,202.7C840,203,960,181,1080,160C1200,139,1320,117,1380,106.7L1440,96L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path>
 </svg>
 </div>
-        <div className="flex item-center justify-between mx-auto max-w-7xl h-16 mt-5 sm:flex items-center">
-          <div>
-            <h1 className="text-2xl font-bold ml-4 ">
-            Career<span className=" text-white">Nest</span>
+<div className="flex item-center justify-between mx-auto max-w-7xl h-16 mt-5 sm:flex items-center">
+          <div className="flex">
+          <img src={carrernest} alt="logo" className="h-14 rounded-full"/>
+            <h1 className="text-2xl font-bold ml-4 mt-3 ">
+              
+            Career<span className="text-white">Nest</span>
             </h1>
+          
           </div>
           <div className="flex items-center gap-12 text-white">
             <ul className="flex font-medium items-center gap-5 cursor-pointer ml-3 pl-5">
